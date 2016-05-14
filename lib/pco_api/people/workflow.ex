86405e524a0e77222ffda14eabfa601e13e2488b
Jesse J. Anderson <jessejanderson@gmail.com>
2016-05-14 10:49:46 -0700

    refactor(Workflow):  pass params through

--- lib/pco_api/people/workflow.ex
+++ lib/pco_api/people/workflow.ex
@@ -4,58 +4,58 @@ defmodule PcoApi.People.Workflow do
   """
 
   # Activity
-  def get(id, card_id: card_id, activity_id: activity_id) do
+  def get(id, [{:card_id, card_id}, {:activity_id, activity_id} | params]) do
     id <> "/cards/" <> card_id <> "/activities/" <> activity_id
-    |> get([])
+    |> get(params)
   end
 
   # Note
-  def get(id, card_id: card_id, note_id: note_id) do
+  def get(id, [{:card_id, card_id}, {:note_id, note_id} | params]) do
     id <> "/cards/" <> card_id <> "/notes/" <> note_id
-    |> get([])
+    |> get(params)
   end
 
   # Task
-  def get(id, card_id: card_id, task_id: task_id) do
+  def get(id, [{:card_id, card_id}, {:task_id, task_id} | params]) do
     id <> "/cards/" <> card_id <> "/tasks/" <> task_id
-    |> get([])
+    |> get(params)
   end
 
   # Card
-  def get(id, card_id: card_id) do
+  def get(id, [{:card_id, card_id} | params]) do
     id <> "/cards/" <> card_id
-    |> get([])
+    |> get(params)
   end
 
   # Step
-  def get(id, step_id: step_id) do
+  def get(id, [{:step_id, step_id} | params]) do
     id <> "/steps/" <> step_id
-    |> get([])
+    |> get(params)
   end
 
-  def cards(id) do
+  def cards(id, params \\ []) do
     id <> "/cards"
-    |> get([])
+    |> get(params)
   end
 
-  def activities(id, card_id: card_id) do
+  def activities(id, [{:card_id, card_id} | params]) do
     id <> "/cards/" <> card_id <> "/activities"
-    |> get([])
+    |> get(params)
   end
 
-  def notes(id, card_id: card_id) do
+  def notes(id, [{:card_id, card_id} | params]) do
     id <> "/cards/" <> card_id <> "/notes"
-    |> get([])
+    |> get(params)
   end
 
-  def steps(id) do
+  def steps(id, params \\ []) do
     id <> "/steps"
-    |> get([])
+    |> get(params)
   end
 
-  def tasks(id, card_id: card_id) do
+  def tasks(id, [{:card_id, card_id} | params]) do
     id <> "/cards/" <> card_id <> "/tasks"
-    |> get([])
+    |> get(params)
   end
 
   use PcoApi.Actions
