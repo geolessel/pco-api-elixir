8dd6286fbd02a3bc24681cb1fda01b4f27e9ed93
Jesse J. Anderson <jessejanderson@gmail.com>
2016-05-23 13:03:20 -0700

    Cleanup Docs for Workflow Card and Activity

--- lib/pco_api/people/workflow/card.ex
+++ lib/pco_api/people/workflow/card.ex
@@ -16,43 +16,40 @@ defmodule PcoApi.People.Workflow.Card do
   linked_association :notes
 
   @doc """
-  Gets associated Workflow.Card records from a Workflow Record from links.
+  Gets associated WorkflowCard records from a Workflow Record from links.
 
   ## Example:
 
-      iex> %PcoApi.Record{type: "Person", links: %{"addresses" => "http://example.com"}} |> PcoApi.People.Address.get
-      %PcoApi.Record{type: "Address", ...}
+      iex> %PcoApi.Record{type: "Workflow", links: %{"cards" => "http://example.com"}} |> Card.get
+      %PcoApi.Record{type: "WorkflowCard", ...}
 
   """
-  # def get(%PcoApi.Record{type: "Person", links: %{"addresses" => url}}), do: get url
   def get(%PcoApi.Record{type: "Workflow", links: %{"cards" => url}}), do: get url
 
   @doc """
-  Gets associated Address records from a Person Record when no address link is found.
+  Gets associated WorkflowCard records from a Workflow Record when no cards link is found.
 
-  Sometimes a record my not include an address link. This function recreates a URL to
-  get the associated records just based off of the Person id.
+  Sometimes a record my not include an cards link. This function recreates a URL to
+  get the associated records just based off of the Workflow Id.
 
   ## Example:
 
-      iex> %PcoApi.Record{type: "Person", id: 1} |> PcoApi.People.Address.get
-      %PcoApi.Record{type: "Address", id: 1, ...}
+      iex> %PcoApi.Record{type: "Workflow", id: 1} |> Card.get
+      %PcoApi.Record{type: "WorkflowCard", id: 1, ...}
 
   """
-  # def get(%PcoApi.Record{type: "Person", id: id}), do: get("people/#{id}/addresses")
   def get(%PcoApi.Record{type: "Workflow", id: id}), do: get("#{id}/cards")
 
   @doc """
-  Gets a single Address for a Person.
+  Gets a single WorkflowCard for a Workflow.
 
-  Requires a Person with an ID and an Address Id.
+  Requires a Workflow Record with an ID and a WorkflowCard Id.
 
   ## Example:
 
-      iex> %PcoApi.Record{type: "Person", id: 1} |> Address.get(2)
-      %PcoApi.Record{type: "Address", id: 2} # for Person.id == 1
+      iex> %PcoApi.Record{type: "Workflow", id: 1} |> Card.get(2)
+      %PcoApi.Record{type: "WorkflowCard", id: 2} # for Workflow.id == 1
 
   """
-  # def get(%PcoApi.Record{type: "Person", id: person_id}, id), do: get("people/#{person_id}/addresses/#{id}")
   def get(%PcoApi.Record{type: "Workflow", id: workflow_id}, id), do: get("#{workflow_id}/cards/#{id}")
 end
