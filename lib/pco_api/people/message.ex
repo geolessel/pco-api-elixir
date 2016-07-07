4a4dc6b82be4aa2ab2a10931808d0b98dbcf7569
Geoffrey Lessel <geo@geoffreylessel.com>
2016-07-06 19:30:38 -0700

    Add message

---
+++ lib/pco_api/people/message.ex
@@ -0,0 +1,11 @@
+defmodule PcoApi.People.Message do
+  use PcoApi.Actions, only: [:list, :get]
+
+  import PcoApi.RecordAssociation
+  linked_association :message_group
+  linked_association :to
+
+  def list(params) when is_list(params), do: get(params, "messages")
+
+  def get(id) when is_integer(id), do: get("messages/#{id}")
+end
---
+++ test/pco_api/people/message_test.exs
@@ -0,0 +1,59 @@
+defmodule PcoApi.People.MessageTest do
+  use ExUnit.Case, async: false
+  alias PcoApi.People.Message
+  alias TestHelper.Fixture
+
+  setup do
+    bypass = Bypass.open
+    Application.put_env(:pco_api, :endpoint_base, "http://localhost:#{bypass.port}/")
+    {:ok, bypass: bypass}
+  end
+
+  test ".list requests the v2 endpoint", %{bypass: bypass} do
+    Bypass.expect bypass, fn conn ->
+      assert "/people/v2/messages" == conn.request_path
+      assert "GET" == conn.method
+      Plug.Conn.resp(conn, 200, Fixture.dummy)
+    end
+    Message.list
+  end
+
+  test ".get(id) returns a single record", %{bypass: bypass} do
+    Bypass.expect bypass, fn conn ->
+      assert "/people/v2/messages/1" == conn.request_path
+      assert "GET" == conn.method
+      Plug.Conn.resp(conn, 200, Fixture.dummy)
+    end
+    Message.get(1)
+  end
+
+  test ".message_group requests the message group", %{bypass: bypass} do
+    Bypass.expect bypass, fn conn ->
+      assert "/people/v2/messages/1/message_group" == conn.request_path
+      assert "GET" == conn.method
+      Plug.Conn.resp(conn, 200, Fixture.dummy)
+    end
+    message |> Message.message_group
+  end
+
+  test ".to requests the message recipient", %{bypass: bypass} do
+    Bypass.expect bypass, fn conn ->
+      assert "/people/v2/messages/1/to" == conn.request_path
+      assert "GET" == conn.method
+      Plug.Conn.resp(conn, 200, Fixture.dummy)
+    end
+    message |> Message.to
+  end
+
+  def message do
+    url_root = "https://api.planningcenteronline.com/people/v2/messages/1"
+    %PcoApi.Record{
+      type: "Message",
+      id: "1",
+      links: %{
+        "message_group" => url_root <> "/message_group",
+        "to" => url_root <> "/to"
+      }
+    }
+  end
+end
