90e70a4b704fd30f0b5d36ae68a2cd94186d4de9
Jesse J. Anderson <jessejanderson@gmail.com>
2016-08-02 17:08:15 -0700

    let actions.new accept maps

--- lib/pco_api/actions/new.ex
+++ lib/pco_api/actions/new.ex
@@ -3,6 +3,10 @@ defmodule PcoApi.Actions.New do
     quote do
       import PcoApi.Actions.New
 
+      def new(attrs, type) when is_map(attrs) and is_binary(type) do
+        %PcoApi.Record{attributes: attrs, type: type}
+      end
+
       def new(attrs, type) when is_list(attrs) and is_binary(type) do
         attrs_map = Enum.into(attrs, %{}, fn({k,v}) -> {Atom.to_string(k), v} end)
         %PcoApi.Record{attributes: attrs_map, type: type}
